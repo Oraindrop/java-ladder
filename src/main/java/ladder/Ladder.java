91323b5b245febcfc103afcafe439b354c841f08
LAPTOP-E3LTPJD6\ChoiSing <chltmdals115@gmail.com>
2018-09-16 21:52:19 +0900

    add part of function

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -1,4 +1,44 @@
 package ladder;
 
+import java.util.Random;
+
 public class Ladder {
+    private static final int RANDOM_BOUND = 2;
+    private static final int ZERO = 0;
+    private static final String SPACE = " ";
+    private static final String HYPHEN = "-";
+    private static final String WALL = "|";
+
+    private static Random rnd = new Random();
+    private static StringBuilder sb = new StringBuilder();
+
+    private int numberOfPlayer;
+    private String representLadder;
+
+    public Ladder(int numberOfPlayer) {
+        this.numberOfPlayer = numberOfPlayer;
+        this.representLadder = this.makeRepresentLadder();
+    }
+
+    public String decideLine(){
+        int randomValue = rnd.nextInt(RANDOM_BOUND);
+        return (randomValue == ZERO) ? SPACE : HYPHEN;
+    }
+
+    public String makeRepresentLadder(){
+        sb.setLength(ZERO);
+        sb.append(WALL);
+        for(int i = ZERO; i < this.numberOfPlayer - 1; i++){
+            sb.append(this.decideLine());
+            sb.append(WALL);
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "Ladder{" +
+                "representLadder='" + representLadder + '\'' +
+                '}';
+    }
 }
--- src/main/java/ladder/LadderGame.java
+++ src/main/java/ladder/LadderGame.java
@@ -1,7 +1,25 @@
 package ladder;
 
-public class LadderGame {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class LadderGame {
+    List<Ladder> ladders;
+
+    public LadderGame(){
+        this.ladders = new ArrayList<>();
+    }
+
+    public void gameStart(int numberOfPlayer, int numberOfLadder){
+        for(int i = 0; i < numberOfLadder; i++){
+            this.ladders.add(new Ladder(numberOfPlayer));
+        }
+        System.out.println(this.ladders);
+    }
+
+
+    public static void main(String[] args) {
+        LadderGame ladderGame = new LadderGame();
+        ladderGame.gameStart(InputView.inputNumberOfPlayer(), InputView.inputHeightOfLadder());
     }
 }
